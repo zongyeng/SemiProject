22dbbb633fabba0b8256349f3e4be524da7d7cbb
sincerely008 <sincerely008@gmail.com>
2019-07-22 21:35:54 +0900

    asd

--- src/semi/controller/ControllerMoving.java
+++ src/semi/controller/ControllerMoving.java
@@ -2,7 +2,7 @@ package semi.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
+
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,10 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import semi.dao.Util;
 import semi.dao.DaoDatabase1;
-import semi.dto.DtoDatabase1;
-import semi.dto.calDto;
 
 
 @WebServlet("/cal.do")
@@ -35,57 +32,18 @@ public class ControllerMoving extends HttpServlet {
 		String command = request.getParameter("command");
 		System.out.println("["+command+"]");
 		
-		if(command.equals("calendar")) {
-			response.sendRedirect("calendar.jsp");
-		} else if(command.equals("calendar2")) {
-			response.sendRedirect("CalendarExam.jsp");
-		} else if(command.equals("insertcal")) {
-			int res = 0;
-			String id = request.getParameter("id");
-			
-			String title = request.getParameter("title");
-			String content = request.getParameter("content");
-			
-			String year = request.getParameter("year");
-			String month = request.getParameter("month");
-			String date = request.getParameter("date");
-			String hour = request.getParameter("hour");
-			String min = request.getParameter("min");
-			
-			String mdate = year+Util.isTwo(month)+Util.isTwo(date)+Util.isTwo(hour)+Util.isTwo(min);
-					
-			DtoDatabase1 dto = new DtoDatabase1();
-			dto.setId(id);
-			dto.setTitle(title);
-			dto.setContent(content);
-			dto.setMdate(mdate);
-					
-			res = dao.insertCalBoard(dto);
-			
-			if(res > 0) {
-				alert(response, "일정 추가 성공!", "cal.do?command=calendar");
-			} else {
-				dispatch("error.jsp", request,response);
-			}
-		} else if(command.equals("delete")) {
-			int seq = Integer.parseInt(request.getParameter("seq"));
-			
-			
-		} else if(command.equals("callist")) {
-			String year = request.getParameter("year");
-			String month = request.getParameter("month");
-			String date = request.getParameter("date");
-			
-			String yyyyMMdd = year + Util.isTwo(month)+Util.isTwo(date);
-			
-			List<DtoDatabase1> list = dao.getCalList("kh", yyyyMMdd);
-			request.setAttribute("list", list);
-			dispatch("callist.jsp", request, response);
-			
-		} else if(command.equals("calviewlist")){
-			
-		} else if(command.equals("selectone")) {
-			
+		if(command.equals("login")) {
+		} else if(command.equals("logout")) {
+		} else if(command.equals("userinfo")) {
+		} else if(command.equals("userupdate")) {
+		} else if(command.equals("deleteuser")) {
+		} else if(command.equals("registform")){
+		} else if(command.equals("idchk")) {
+		} else if(command.equals("alluser")){
+		} else if(command.equals("enableduser")){
+		}else if(command.equals("createuser")){
+		}else if(command.equals("page")){
+		}else if(command.equals("updateroleform")){
 		}
 		
 	}
