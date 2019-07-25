eb2fff8d80cfed3110859f1c0fcddb8bbef0b0c4
김재석 <xmfnajs123@gmail.com>
2019-07-25 21:03:09 +0900

    music file 추가

---
+++ src/semi/dto/MusicDto.java
@@ -0,0 +1,21 @@
+package semi.dto;
+
+public class MusicDto {
+	
+	private int seq;
+	private int musicianseq;
+	private int no;
+	private String name;
+	private int size;
+	private int time;
+	private int highlitrtime;
+	private String content;
+	private String composer;
+	private String lylicist;
+	private Date inputdate;
+	private String Tag;
+	private char enable;
+	private String album;
+	
+
+}
